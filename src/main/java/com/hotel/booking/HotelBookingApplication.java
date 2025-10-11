49fdfc0cdad37d9222dc67c27a68d2553d7cd756
Denis <deniskatcko05@gmail.com>
2025-10-11 19:54:35 +0300

    start

---
+++ src/main/java/com/hotel/booking/HotelBookingApplication.java
@@ -0,0 +1,9 @@
+package com.hotel.booking;
+
+import org.springframework.boot.SpringApplication;
+
+class HotelBookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingApplication.class, args);
+    }
+}
